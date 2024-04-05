ab42628cda0dce8ca2964d56733d850212a25a94
Rohan Sonawane <147150954+geekfreak123@users.noreply.github.com>
2024-04-05 02:43:17 +0000

    Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
-export default function App() {
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch("https://restcountries.com/v3.1/all");
+      if (!response.ok) {
+        throw new Error("Failed to fetch countries");
+      }
+      const data = await response.json();
+      setCountries(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value.toLowerCase());
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearch}
+        className="search-input"
+      />
+      <div id="countryContainer">
+        {countries
+          .filter((country) =>
+            country.name.common.toLowerCase().includes(searchTerm),
+          )
+          .map((country) => (
+            <div key={country.name.common} className="countryCard">
+              <img
+                src={country.flags.png}
+                alt={`${country.name.common} flag`}
+              />
+              <span>{country.name.common}</span>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
+
+export default App;
